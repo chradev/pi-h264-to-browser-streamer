9a7978f35ffb8ac0d6ee694c53f5e4c7f4c5d070
Christo Radev <christo.radev@gmail.com>
2024-05-09 14:18:48 +0300

    Updates for basic changes

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
+# Motivation for extension
+
+The parent project is an excellent solution for near-real-time video capturing, encoding and streaming. Extending it to reach the robot's stereo vision requirements is a challenge that could be solved thanks to advances in embedded and mobile devices supporting video processing with hardware acceleration.
+
+The main reasons for extending the project are:
+  * The new PRi 5 board is much more powerful and supports two upto 16 MP CSI cameras;
+  * There is a feature-rich set of picamera2 library supporting the video processing;
+  * Extremely low latency is a main reason for choosing the project for stereo vision of a robot.
+
+Notes: Requirements are like in [rpi5-h264-live-stereo-streamer](https://github.com/chradev/rpi5-h264-live-stereo-streamer/) but include following one:
+ * The streaming server has to be able to change picamera2 properties like "ScalerCrop" for implementing pan & tilt eye movements.
+
+# Basic changes in:
+ * src/server.py
+   - changes to send to web page: {'port': serverPort, 'width': frameWidth, 'height': frameHeight, 'fps': frameRate}
+   - create 2x rpicamera2 instances
+   - dublicate: StreamingOutput and wsHandler classes for bulding two video paths
+   - add logic to move range-of-interest for both cameras in StreamingOutput objects
+   - adding requestHandlers for both cam0 and cam1 streams
+   - initialize classs, build videf paths and start cameras
+ * src/index.html
+   - define two <video></video> elements
+   - instantiate two JMuxer classes
+   - create and connect two WebSocket for visualization of both camera streams
+
+# Problems not solved for now:
+ * dublication of StreamingOutput and wsHandler classes
+ * using of offsetX and offsetY variables calculated by independent process
+
+# Basic changes usage
+ * run in src: python3 server.py
+ * browse: http://RPi-IP:8000/
+ * watch moving image in X direction of both RPi 5 cameras streams
+
+# Motivation of the original project
+
 Fork of [dans98/pi-h264-to-browser](https://github.com/dans98/pi-h264-to-browser/) that supports the new picamera2 Python library.
 
 ### About
--- src/server.py
+++ src/server.py
@@ -7,17 +7,25 @@ from threading import Condition
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import Output
 
+from libcamera import Transform
+
 # start configuration
 serverPort = 8000
-wsURL = "ws://my_ip/ws/"
-framerate = 30
+frameRate = 30
+frameWidth = 960
+frameHeight = 1080
+frameTransform = 180
 
-picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)}))
+picam20 = Picamera2(0)
+picam21 = Picamera2(1)
+
+picam20.configure(picam20.create_video_configuration(main={"size": (frameWidth, frameHeight)}, transform=Transform(frameTransform) ))
+picam21.configure(picam21.create_video_configuration(main={"size": (frameWidth, frameHeight)}, transform=Transform(frameTransform) ))
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -36,28 +44,93 @@ def templatize(content, replacements):
     return tmpl.substitute(replacements)
 
 
-indexHtml = templatize(getFile('index.html'), {'wsurl': wsURL, 'fps': framerate})
+indexHtml  = templatize(getFile('index.html'), {'port': serverPort, 'width': frameWidth, 'height': frameHeight, 'fps': frameRate})
 jmuxerJs = getFile('jmuxer.min.js')
 
-
-class StreamingOutput(Output):
+class StreamingOutput0(Output):
     def __init__(self):
         super().__init__()
         self.loop = None
         self.buffer = io.BytesIO()
+        self.offsetX = 0
+        self.offmaxX = 2320
+        self.crop = (1160, 692, 960, 1080)
 
     def setLoop(self, loop):
         self.loop = loop
 
     def outputframe(self, frame, keyframe=True, timestamp=None):
         self.buffer.write(frame)
-        if self.loop is not None and wsHandler.hasConnections():
-            self.loop.add_callback(callback=wsHandler.broadcast, message=self.buffer.getvalue())
+        if self.loop is not None and ws0Handler.hasConnections():
+            self.loop.add_callback(callback=ws0Handler.broadcast, message=self.buffer.getvalue())
         self.buffer.seek(0)
         self.buffer.truncate()
+        if self.offsetX == self.offmaxX:
+            self.offsetX = 0
+        else:
+            self.offsetX = self.offsetX + 1
+        self.crop = (self.offsetX, 360, 960, 1080)
+        picam20.set_controls({"ScalerCrop": self.crop})
+
+class StreamingOutput1(Output):
+    def __init__(self):
+        super().__init__()
+        self.loop = None
+        self.buffer = io.BytesIO()
+        self.offsetX = 620
+        self.offmaxX = 2320
+        self.crop = (1160, 692, 960, 1080)
+
+    def setLoop(self, loop):
+        self.loop = loop
+
+    def outputframe(self, frame, keyframe=True, timestamp=None):
+        self.buffer.write(frame)
+        if self.loop is not None and ws1Handler.hasConnections():
+            self.loop.add_callback(callback=ws1Handler.broadcast, message=self.buffer.getvalue())
+        self.buffer.seek(0)
+        self.buffer.truncate()
+        if self.offsetX == self.offmaxX:
+            self.offsetX = 0
+        else:
+            self.offsetX = self.offsetX + 1
+        self.crop = (self.offsetX, 360, 960, 1080)
+        picam21.set_controls({"ScalerCrop": self.crop})
 
 
-class wsHandler(tornado.websocket.WebSocketHandler):
+class ws0Handler(tornado.websocket.WebSocketHandler):
+    connections = []
+
+    def open(self):
+        self.connections.append(self)
+
+    def on_close(self):
+        self.connections.remove(self)
+
+    def on_message(self, message):
+        pass
+
+    @classmethod
+    def hasConnections(cl):
+        if len(cl.connections) == 0:
+            return False
+        return True
+
+    @classmethod
+    async def broadcast(cl, message):
+        for connection in cl.connections:
+            try:
+                await connection.write_message(message, True)
+            except tornado.websocket.WebSocketClosedError:
+                pass
+            except tornado.iostream.StreamClosedError:
+                pass
+
+    def check_origin(self, origin):
+        return True
+
+
+class ws1Handler(tornado.websocket.WebSocketHandler):
     connections = []
 
     def open(self):
@@ -99,24 +172,31 @@ class jmuxerHandler(tornado.web.RequestHandler):
         self.set_header('Content-Type', 'text/javascript')
         self.write(jmuxerJs)
 
-
 requestHandlers = [
-    (r"/ws/", wsHandler),
+    (r"/cam0/", ws0Handler),
+    (r"/cam1/", ws1Handler),
     (r"/", indexHandler),
     (r"/jmuxer.min.js", jmuxerHandler)
 ]
 
 try:
-    output = StreamingOutput()
-    encoder = H264Encoder(repeat=True, framerate=framerate, qp=23)
-    encoder.output = output
-    picam2.start_recording(encoder, output)
+    output0 = StreamingOutput0()
+    output1 = StreamingOutput1()
+    encoder0 = H264Encoder(repeat=True, framerate=frameRate, qp=23)
+    encoder1 = H264Encoder(repeat=True, framerate=frameRate, qp=23)
+    encoder0.output = output0
+    encoder1.output = output1
+    picam20.start_recording(encoder0, output0)
+    picam21.start_recording(encoder1, output1)
 
     application = tornado.web.Application(requestHandlers)
     application.listen(serverPort)
     loop = tornado.ioloop.IOLoop.current()
-    output.setLoop(loop)
+    output0.setLoop(loop)
+    output1.setLoop(loop)
     loop.start()
 except KeyboardInterrupt:
-    picam2.stop_recording()
+    picam20.stop_recording()
+    picam21.stop_recording()
     loop.stop()
+
